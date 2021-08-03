19d8fafc587e981a2d86baeeb35ff7ebe32363d7
Russell Gordon <rgordon@lcs.on.ca>
2021-08-03 11:48:14 -0400

    Initial Commit

---
+++ HeteroTaskGroupChallenge/HeteroTaskGroupChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeteroTaskGroupChallengeApp.swift
+//  HeteroTaskGroupChallenge
+//
+//  Created by Russell Gordon on 2021-08-03.
+//
+
+import SwiftUI
+
+@main
+struct HeteroTaskGroupChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
